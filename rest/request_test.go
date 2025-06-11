e41951d7da77e78f59c7643b46a3898c22d45fc2
Brendan Bates <batesbrendan@gmail.com>
2025-06-11 22:58:56 +1000

    Add test for global path param

--- rest/request_test.go
+++ rest/request_test.go
@@ -50,6 +50,26 @@ func TestRemovePathParamsFromRequestBody(t *testing.T) {
 	assert.False(t, ok, "Expected tailnet to be removed from the HTTP request body since it is a path param.")
 }
 
+func TestProviderGlobalPathParams(t *testing.T) {
+	ctx := context.Background()
+	testCreateJSONPayload := `{}`
+
+	expectedBaseId := "fake-base-id"
+
+	var inputs map[string]interface{}
+	if err := json.Unmarshal([]byte(testCreateJSONPayload), &inputs); err != nil {
+		t.Fatalf("Failed to unmarshal test payload: %v", err)
+	}
+
+	p := makeTestGenericProvider(ctx, t, nil)
+	p.(*Provider).GetGlobalPathParams()["baseId"] = expectedBaseId
+
+	httpReq, err := p.(Request).CreatePostRequest(ctx, "/v2/{baseId}/fakeresource", []byte(testCreateJSONPayload), resource.NewPropertyMapFromMap(inputs))
+	assert.Nil(t, err)
+	assert.NotNil(t, httpReq)
+	assert.Equal(t, httpReq.URL.Path, "/v2/"+expectedBaseId+"/fakeresource")
+}
+
 func TestLastPathParamIsResourceId(t *testing.T) {
 	ctx := context.Background()
 

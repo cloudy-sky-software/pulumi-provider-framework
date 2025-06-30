cf50d748f024d950b5b53e5f47b969b022091d2d
Brendan Bates <batesbrendan@gmail.com>
2025-06-30 22:32:57 +1000

    Add GetGlobalPathParam callback func to ProviderCallback interface, use in Configure method of provider, add UnimplementedProviderCallback type, modify test fixture methods that create fake providers to allow a callback fixture arg, if required

--- callback/callback.go
+++ callback/callback.go
@@ -2,6 +2,7 @@ package callback
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -18,6 +19,10 @@ type ProviderCallback interface {
 	// OnConfigure is a hook for configuring the provider.
 	// Return a non-nil response to override the default behavior.
 	OnConfigure(ctx context.Context, req *pulumirpc.ConfigureRequest) (*pulumirpc.ConfigureResponse, error)
+
+	// GetGlobalPathParams returns a map of path parameters used across all (or most) requests
+	// This method is called during the Configure phase, but after the OnConfigure hook.
+	GetGlobalPathParams(ctx context.Context, req *pulumirpc.ConfigureRequest) (map[string]string, error)
 
 	OnPreInvoke(ctx context.Context, req *pulumirpc.InvokeRequest, httpReq *http.Request) error
 	OnPostInvoke(ctx context.Context, req *pulumirpc.InvokeRequest, outputs interface{}) (map[string]interface{}, error)
@@ -63,4 +68,62 @@ type ProviderCallback interface {
 	OnPreDelete(ctx context.Context, req *pulumirpc.DeleteRequest, httpReq *http.Request) error
 	// OnPostDelete is a hook for modifying the outputs.
 	OnPostDelete(ctx context.Context, req *pulumirpc.DeleteRequest) error
+}
+
+type UnimplementedProviderCallback struct{}
+
+func (UnimplementedProviderCallback) GetAuthorizationHeader() string {
+	return ""
+}
+
+func (UnimplementedProviderCallback) OnConfigure(context.Context, *pulumirpc.ConfigureRequest) (*pulumirpc.ConfigureResponse, error) {
+	return nil, fmt.Errorf("OnConfigure not implemented")
+}
+
+func (UnimplementedProviderCallback) GetGlobalPathParams(context.Context, *pulumirpc.ConfigureRequest) (map[string]string, error) {
+	return nil, fmt.Errorf("OnConfigure not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPreInvoke(context.Context, *pulumirpc.InvokeRequest, *http.Request) error {
+	return fmt.Errorf("OnPreInvoke not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPostInvoke(context.Context, *pulumirpc.InvokeRequest, interface{}) (map[string]interface{}, error) {
+	return nil, fmt.Errorf("OnPostInvoke not implemented")
+}
+
+func (UnimplementedProviderCallback) OnDiff(context.Context, *pulumirpc.DiffRequest, string, *resource.ObjectDiff, *openapi3.MediaType) (*pulumirpc.DiffResponse, error) {
+	return nil, fmt.Errorf("OnDiff not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPreCreate(context.Context, *pulumirpc.CreateRequest, *http.Request) error {
+	return fmt.Errorf("OnPreCreate not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPostCreate(context.Context, *pulumirpc.CreateRequest, interface{}) (map[string]interface{}, error) {
+	return nil, fmt.Errorf("OnPostCreate not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPreRead(context.Context, *pulumirpc.ReadRequest, *http.Request) error {
+	return fmt.Errorf("OnPreRead not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPostRead(context.Context, *pulumirpc.ReadRequest, interface{}) (map[string]interface{}, error) {
+	return nil, fmt.Errorf("OnPostRead not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPreUpdate(context.Context, *pulumirpc.UpdateRequest, *http.Request) error {
+	return fmt.Errorf("OnPreUpdate not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPostUpdate(context.Context, *pulumirpc.UpdateRequest, http.Request, interface{}) (map[string]interface{}, error) {
+	return nil, fmt.Errorf("OnPostUpdate not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPreDelete(context.Context, *pulumirpc.DeleteRequest, *http.Request) error {
+	return fmt.Errorf("OnPreDelete not implemented")
+}
+
+func (UnimplementedProviderCallback) OnPostDelete(context.Context, *pulumirpc.DeleteRequest) error {
+	return fmt.Errorf("OnPostDelete not implemented")
 }
